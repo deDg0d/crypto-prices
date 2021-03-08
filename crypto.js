832fd9d6e880b57688eec93c0d033baccc04cb62
dani <danielramezany@yahoo.com>
2021-03-08 20:27:26 +0430

    app added

---
+++ crypto.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const http = require('http')
+var crypto = []
+//axios.get('https://www.coindesk.com/price/bitcoin')
+//.then(res=>{
+   // $ = cheerio.load(res.data)
+   // const bitcoin = $('div.price-large').text()
+//console.log(bitcoin)
+//})
+axios.get('https://coinmarketcap.com/')
+.then(res=>{
+    
+    $ = cheerio.load(res.data)
+    
+   var price  = $('div.price___3rj7O ').each((i,e)=>{
+   price = $(e).text()
+   crypto[i]= {price}
+})
+
+console.log('bitcoin :' + crypto[0]+'\n'+ 'ethereum :' + crypto[1])
+})
